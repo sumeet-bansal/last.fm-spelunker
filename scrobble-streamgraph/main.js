0d7de960642c8cc964d1c6ab3cf0fb52aaf66e9b
Sumeet Bansal <sumeetbansal@gmail.com>
2017-10-08 00:47:03 -0700

    updated opacity for mouse overs

--- scrobble-streamgraph/main.js
+++ scrobble-streamgraph/main.js
@@ -98,7 +98,7 @@ function chart(csvpath, color) {
             svg.selectAll(".layer").transition()
             .duration(250)
             .attr("opacity", function(d, j) {
-                return j != i ? 0.6 : 1;
+                return j != i ? 0.3 : 1;
             })})
 
             .on("mousemove", function(d, i) {
