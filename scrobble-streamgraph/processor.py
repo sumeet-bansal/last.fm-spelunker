1c9e3a9d5c3f22232968c2668bf3cf74572a0ff8
Sumeet Bansal <sumeetbansal@gmail.com>
2017-10-11 02:30:47 -0700

    modified streamgraph to process timeframes past a single year

--- scrobble-streamgraph/processor.py
+++ scrobble-streamgraph/processor.py
@@ -1,6 +1,8 @@
 import sys
 import dataset
 from datetime import datetime
+from dateutil.rrule import rrule, MONTHLY
+from dateutil.relativedelta import relativedelta
 
 # chart properties
 size = 2000
@@ -10,6 +12,10 @@ metric = 'artist_text'
 # gets all artists and their respective daily play counts
 db = dataset.connect('sqlite:///../last-fm.db')
 total = db['scrobbles'].count()
+timeframe = db.query('SELECT MIN(date_uts), MAX(date_uts) FROM scrobbles').next()
+mintime = datetime.fromtimestamp(timeframe['MIN(date_uts)'])
+maxtime = datetime.fromtimestamp(timeframe['MAX(date_uts)'])
+timeframe = len([dt for dt in rrule(MONTHLY, dtstart=mintime, until=maxtime)])
 sql = 'SELECT {0}, date_uts, count({0}) FROM scrobbles GROUP BY {0}, date_year, date_month HAVING count({0}) > {1}'.format(metric, stream_limit)
 result = db.query(sql)
 
@@ -17,15 +23,17 @@ streams = {}
 for row in result:
 	artist = row[metric]
 	if artist not in streams:
-		streams[artist] = [0 for i in range(12)]
-	dt = datetime.fromtimestamp(int(row['date_uts']))
-	streams[artist][int(dt.month) - 1] = row['count(%s)' % metric]
+		streams[artist] = [0 for i in range(timeframe)]
+	current = datetime.fromtimestamp(int(row['date_uts']))
+	elapsed = len([dt for dt in rrule(MONTHLY, dtstart=mintime, until=current)])
+	streams[artist][elapsed - 1] = row['count(%s)' % metric]
 
 with open('stream-data.csv', 'w') as csv:
 	csv.write('key,value,date\n')
-	for i in range(12):
+	for i in range(timeframe):
+		current = mintime + relativedelta(months=i)
 		for artist in streams:
 			try:
-				csv.write('%s,%s,%s\n' % (artist.replace(',', ''), streams[artist][i], '%s/01/17' % str(i + 1)))
+				csv.write('%s,%s,%s\n' % (artist.replace(',', ''), streams[artist][i], '%s/01/%s' % (current.month, str(current.year)[2:])))
 			except UnicodeEncodeError:
 				pass
