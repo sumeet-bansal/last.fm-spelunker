f3b0558c03ef3331173f03c4c15724d769b48b68
Sumeet Bansal <sumeetbansal@gmail.com>
2018-03-15 18:19:27 -0700

    made scraper its own class and added an 'update' feature
    'update' feature updates an existing database with all scrobbles since
    most recent one in the database

--- scraper.py
+++ scraper.py
@@ -7,159 +7,189 @@ import dataset
 from datetime import datetime
 import scrubber
 
-# constants
-try:
-	USER = sys.argv[1]
-except IndexError:
-	print("[ERROR] No last.fm username specified.")
-	quit()
-API_KEY = '2bf228a2ce0167b5b857dd53ea6f39c1'
-ROOT_URL = 'https://ws.audioscrobbler.com/2.0/'
-PER_PAGE = 200
-RECENT_URL = ROOT_URL + '/?method=user.getrecenttracks&user=' + USER + '&api_key=' + API_KEY + '&format=json&page=%s&limit=%s'
-ARTIST_URL = ROOT_URL + '/?method=artist.getinfo&api_key=' + API_KEY + '&artist=%s&format=json'
+class Scraper:
 
-# flattens the JSON into a uniform depth (i.e. a series of keys and non-nested values)
-# modified from http://stackoverflow.com/a/6027615/254187 to strip pound symbols
-def flatten(d, parent_key=''):
-	items = []
-	for k, v in d.items():
-		new_key = parent_key + '_' + k if parent_key else k
-		if isinstance(v, collections.MutableMapping):
-			items.extend(flatten(v, new_key).items())
+	def __init__(self, username):
+		self.API_KEY = '2bf228a2ce0167b5b857dd53ea6f39c1'
+		self.ROOT_URL = 'https://ws.audioscrobbler.com/2.0/'
+		self.PER_PAGE = 200		# max allowed by last.fm
+		self.RECENT_URL = self.ROOT_URL + '/?method=user.getrecenttracks&user=' + username
+		self.RECENT_URL += '&api_key=' + self.API_KEY + '&format=json&page=%s&limit=%s'
+		self.ARTIST_URL = self.ROOT_URL + '/?method=artist.getinfo&api_key=' + self.API_KEY + '&artist=%s&format=json'
+
+	# flattens the JSON into a uniform depth (i.e. a series of keys and non-nested values)
+	# modified from http://stackoverflow.com/a/6027615/254187 to strip pound symbols
+	def flatten(self, d, parent_key=''):
+		items = []
+		for k, v in d.items():
+			new_key = parent_key + '_' + k if parent_key else k
+			if isinstance(v, collections.MutableMapping):
+				items.extend(self.flatten(v, new_key).items())
+			else:
+				new_key = new_key.replace('#', '')
+				items.append((new_key, v))
+		return dict(items)
+
+	# removes all matching properties in the `props` list from the json object
+	def clean(self, json, props):
+		for prop in props:
+			if prop in json:
+				del json[prop]
+		return json
+
+	# processes scrobble for SQL insertion
+	def process_scrobble(self, scrobble):
+
+		# removes unnecessary attributes
+		props = ['image', 'streamable', 'url', '@attr', 'mbid']
+		scrobble = self.clean(scrobble, props)
+
+		# flattens the track JSON
+		flattened = self.flatten(scrobble)
+		for key, val in flattened.items():
+			if val == '':
+				flattened[key] = None
+
+		del flattened['artist_mbid']
+		del flattened['album_mbid']
+
+		flattened['artist'] = flattened.pop('artist_text')
+		flattened['track'] = flattened.pop('name')
+		flattened['album'] = flattened.pop('album_text')
+
+		if 'date_uts' in flattened:
+			flattened['timestamp'] = int(flattened.pop('date_uts'))
+			flattened['timestamp_text'] = flattened.pop('date_text')
+			dt = datetime.fromtimestamp(flattened['timestamp'])
+			flattened['play_year'] = dt.year
+			flattened['play_month'] = dt.month
+			flattened['play_date'] = dt.day
+			flattened['play_hour'] = dt.hour
 		else:
-			new_key = new_key.replace('#', '')
-			items.append((new_key, v))
-	return dict(items)
+			flattened = None
+		return flattened
 
-# removes all matching properties in the `props` list from the json object
-def clean(json, props):
-	for prop in props:
-		if prop in json:
-			del json[prop]
-	return json
+	# processes artist for SQL insertion
+	def process_artist(self, artist):
 
-# processes scrobble for SQL insertion
-def process_scrobble(scrobble):
+		# removes unnecessary attributes
+		props = ['ontour', 'image', 'streamable', 'url', 'bio', 'mbid']
+		artist = self.clean(artist, props)
 
-	# removes unnecessary attributes
-	props = ['image', 'streamable', 'url', '@attr', 'mbid']
-	scrobble = clean(scrobble, props)
+		# compresses related artists to a single value
+		related = None
+		if 'similar' in artist:
+			related = []
+			for relatedArtist in artist['similar']['artist']:
+				relatedArtist = self.process_artist(relatedArtist)
+				related.append(relatedArtist['name'].replace(',', ''))
+			del artist['similar']
 
-	# flattens the track JSON
-	flattened = flatten(scrobble)
-	for key, val in flattened.items():
-		if val == '':
-			flattened[key] = None
+		# compresses artist tags to a single value
+		tags = None
+		if 'tags' in artist:
+			tags = []
+			for tag in artist['tags']['tag']:
+				tags.append(tag['name'])
+			del artist['tags']
 
-	del flattened['artist_mbid']
-	del flattened['album_mbid']
+		# converts all artist stats to ints
+		if 'stats' in artist:
+			for stat in artist['stats']:
+				artist['stats'][stat] = int(artist['stats'][stat])
 
-	flattened['artist'] = flattened.pop('artist_text')
-	flattened['track'] = flattened.pop('name')
-	flattened['album'] = flattened.pop('album_text')
+		# flattens the track JSON
+		flattened = flatten(artist)
+		for key, val in flattened.items():
+			if val == '':
+				flattened[key] = None
 
-	if 'date_uts' in flattened:
-		flattened['timestamp'] = int(flattened.pop('date_uts'))
-		flattened['timestamp_text'] = flattened.pop('date_text')
-		dt = datetime.fromtimestamp(flattened['timestamp'])
-		flattened['play_year'] = dt.year
-		flattened['play_month'] = dt.month
-		flattened['play_date'] = dt.day
-		flattened['play_hour'] = dt.hour
-	else:
-		flattened = None
-	return flattened
+		if related is not None:
+			flattened['related'] = ", ".join(related)
+		if tags is not None:
+			flattened['tags'] = ", ".join(tags)
 
-# processes artist for SQL insertion
-def process_artist(artist):
+		return flattened
 
-	# removes unnecessary attributes
-	props = ['ontour', 'image', 'streamable', 'url', 'bio', 'mbid']
-	artist = clean(artist, props)
+	def get_all_scrobbles(self):
 
-	# compresses related artists to a single value
-	related = None
-	if 'similar' in artist:
-		related = []
-		for relatedArtist in artist['similar']['artist']:
-			relatedArtist = process_artist(relatedArtist)
-			related.append(relatedArtist['name'].replace(',', ''))
-		del artist['similar']
+		# gets total num of pages in last.fm user history
+		resp = requests.get(self.RECENT_URL % (1, self.PER_PAGE)).json()
+		try:
+			TOTAL_PAGES = int(resp['recenttracks']['@attr']['totalPages'])
+		except KeyError:
+			print("[ERROR] No last.fm username specified.")
+			quit()
 
-	# compresses artist tags to a single value
-	tags = None
-	if 'tags' in artist:
-		tags = []
-		for tag in artist['tags']['tag']:
-			tags.append(tag['name'])
-		del artist['tags']
-
-	# converts all artist stats to ints
-	if 'stats' in artist:
-		for stat in artist['stats']:
-			artist['stats'][stat] = int(artist['stats'][stat])
-
-	# flattens the track JSON
-	flattened = flatten(artist)
-	for key, val in flattened.items():
-		if val == '':
-			flattened[key] = None
-
-	if related is not None:
-		flattened['related'] = ", ".join(related)
-	if tags is not None:
-		flattened['tags'] = ", ".join(tags)
-
-	return flattened
-
-def scrobbles():
-
-	# gets total num of pages in last.fm user history
-	resp = requests.get(RECENT_URL % (1, PER_PAGE)).json()
-	try:
-		TOTAL_PAGES = int(resp['recenttracks']['@attr']['totalPages'])
-	except KeyError:
-		print("[ERROR] No last.fm username specified.")
-		quit()
-
-	# adds all scrobbles to a list
-	scrobbles = []
-	for page in range(1, TOTAL_PAGES + 1):
-		scrobbles.extend(requests.get(RECENT_URL % (page, PER_PAGE)).json()['recenttracks']['track'])
-		sys.stdout.write("\rRetrieving scrobble history...\t" + str(page) + " of " + str(TOTAL_PAGES))
-		sys.stdout.flush()
-	print("\rRetrieved scrobble history.")
-
-	# iterates through, processes, and inserts each scrobble
-	with dataset.connect('sqlite:///last-fm.db') as db:
-		for scrobble in scrobbles:
-			processed = process_scrobble(scrobble)
-			if processed is not None:
-				db['scrobbles'].insert(processed)
-
-def artists():
-
-	# grabs and inserts info for all distinct scrobbled artists
-	with dataset.connect('sqlite:///last-fm.db') as db:
-		errors = []
-		result = db['scrobbles'].distinct('artist')
-		sql = 'SELECT COUNT(DISTINCT artist) AS count FROM scrobbles'
-		totalArtists = int(db.query(sql).next()['count'])
-		for index, row in enumerate(result):
-			artist = requests.get(ARTIST_URL % row['artist']).json()
-			sys.stdout.write("\rRetrieving artist info...\t%s of %s" % (str(index), str(totalArtists)))
+		# adds all scrobbles to a list
+		scrobbles = []
+		for page in range(1, TOTAL_PAGES + 1):
+			scrobbles.extend(requests.get(self.RECENT_URL % (page, self.PER_PAGE)).json()['recenttracks']['track'])
+			sys.stdout.write("\rRetrieving scrobble history...\t" + str(page) + " of " + str(TOTAL_PAGES))
 			sys.stdout.flush()
-			try:
-				processed = process_artist(artist['artist'])
-				db['artists'].insert(processed)
-			except KeyError:
-				errors.append(row['artist'])
-		sys.stdout.write("\rRetrieving artist info...\t{0} of {0}".format(str(totalArtists)))
-		sys.stdout.flush()
-		print("\rRetrieved artist info.")
-		if errors:
-			print("\nThe following artists could not be located within the last.fm database: \n  " + "\n  ".join(errors))
+		print("\rRetrieved scrobble history.")
+
+		return scrobbles
+
+	def update_scrobbles(self):
+
+		with dataset.connect('sqlite:///last-fm.db') as db:
+			rts = db.query('SELECT MAX(timestamp) as recent from scrobbles').next()['recent']
+
+		# gets total num of pages in last.fm user history
+		resp = requests.get(self.RECENT_URL % (1, self.PER_PAGE)).json()
+		try:
+			TOTAL_PAGES = int(resp['recenttracks']['@attr']['totalPages'])
+		except KeyError:
+			print("[ERROR] No last.fm username specified.")
+			quit()
+
+		# adds all scrobbles to a list
+		scrobbles = []
+		for page in range(1, TOTAL_PAGES + 1):
+			current = requests.get(self.RECENT_URL % (page, self.PER_PAGE)).json()['recenttracks']['track']
+			if 'date' not in current[0]:
+				current = current[1:]
+			for play in current:
+				if int(play['date']['uts']) > rts:
+					scrobbles.append(play)
+				else:
+					print("\rUpdated scrobble history with %d new scrobbles since." % len(scrobbles))
+					return scrobbles
+			sys.stdout.write("\rUpdating scrobble history...\t%d scrobbles added" % len(scrobbles))
+			sys.stdout.flush()
+
+	def insert_scrobbles(self, scrobbles):
+
+		# iterates through, processes, and inserts each scrobble
+		with dataset.connect('sqlite:///last-fm.db') as db:
+			for scrobble in reversed(scrobbles):
+				processed = self.process_scrobble(scrobble)
+				if processed is not None:
+					db['scrobbles'].insert(processed)
+
+	def artists(self):
+
+		# grabs and inserts info for all distinct scrobbled artists
+		with dataset.connect('sqlite:///last-fm.db') as db:
+			errors = []
+			result = db['scrobbles'].distinct('artist')
+			sql = 'SELECT COUNT(DISTINCT artist) AS count FROM scrobbles'
+			totalArtists = int(db.query(sql).next()['count'])
+			for index, row in enumerate(result):
+				artist = requests.get(ARTIST_URL % row['artist']).json()
+				sys.stdout.write("\rRetrieving artist info...\t%s of %s" % (str(index), str(totalArtists)))
+				sys.stdout.flush()
+				try:
+					processed = self.process_artist(artist['artist'])
+					db['artists'].insert(processed)
+				except KeyError:
+					errors.append(row['artist'])
+			sys.stdout.write("\rRetrieving artist info...\t{0} of {0}".format(str(totalArtists)))
+			sys.stdout.flush()
+			print("\rRetrieved artist info.")
+			if errors:
+				print("\nThe following artists could not be located within the last.fm database: \n  " + "\n  ".join(errors))
 
 if __name__ == "__main__":
 	scrobbles()
